5b401657f3360464c21fe3dcf2c42596219e1693
seba-1511 <seba-1511@hotmail.com>
2014-01-10 19:41:06 +0100

    Added begining of a service.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,25 +1,6 @@
 'use strict';
 
 angular.module('websiteApp')
-    .controller('MainCtrl', function($scope) {
-        var news = [{
-            id: 1,
-            title: 'Exemple 1',
-            author: 'Séb',
-            date: 1389283402,
-            content: 'Voilà un exemple de news. src="http://www.swiss-ski.ch/uploads/pics/Aerni_Luca_07.jpg"',
-        }, {
-            id: 3,
-            title: 'Exemple 3 Longue',
-            author: 'Séb',
-            date: 1389283404,
-            content: 'Voilà un exemple de news un peu plus long. Elle devrait être coupée vers la fin, parce que le texte est décidement trop long.',
-        }, {
-            id: 2,
-            title: 'Exemple 2',
-            author: 'Séb',
-            date: 1389283403,
-            content: 'Voilà un exemple de news.',
-        }, ];
-        $scope.news = news;
+    .controller('MainCtrl', function($scope, News) {
+        $scope.news = News.getNews();
     });
--- app/scripts/services/news.js
+++ app/scripts/services/news.js
@@ -1,6 +1,40 @@
 'use strict';
 
 angular.module('websiteApp')
-  .service('News', function News() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+	.service('News', function News() {
+
+		this.getNews = function(id) {
+			var news = [{
+				id: 0,
+				title: 'Exemple 0',
+				author: 'Séb',
+				date: 1389283401,
+				content: 'Voilà un exemple de news. src="http://www.swiss-ski.ch/uploads/pics/Aerni_Luca_07.jpg"',
+			}, {
+				id: 1,
+				title: 'Exemple 1',
+				author: 'Séb',
+				date: 1389283402,
+				content: 'Voilà un exemple de news. src="http://www.swiss-ski.ch/uploads/pics/Aerni_Luca_07.jpg"',
+			}, {
+				id: 2,
+				title: 'Exemple 2',
+				author: 'Séb',
+				date: 1389283403,
+				content: 'Voilà un exemple de news.',
+			}, {
+				id: 3,
+				title: 'Exemple 3 Longue',
+				author: 'Séb',
+				date: 1389283404,
+				content: 'Voilà un exemple de news un peu plus long. Elle devrait être coupée vers la fin, parce que le texte est décidement trop long.',
+			}, ];
+			if (!id) {
+				return news;
+			} else {
+				return news[id];
+			}
+		};
+
+		
+	});
