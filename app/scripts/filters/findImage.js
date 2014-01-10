0a784364f9811817005a3a75fe423cbed20a270b
seba-1511 <seba-1511@hotmail.com>
2014-01-10 22:53:16 +0100

    anged dates of test news.

--- app/scripts/filters/findImage.js
+++ app/scripts/filters/findImage.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('websiteApp')
-    .filter('findImage', function() {
-        return function(input) {
-            var re = /http:\/\/.+?\.jpg|jpeg/gi;
-            input = re.exec(input);
-            input = !!input ? input[0] : '/images/Sflocon_vecteur_Transparent.png';
-            return input;
-        };
-    });
+	.filter('findImage', function() {
+		return function(input) {
+			var re = /(https?:\/\/.*\.(?:png|jpg|svg|jpeg))(?![>|<|'|"])/gi;
+			input = re.exec(input);
+			input = !! input ? input[0] : '/images/Sflocon_vecteur_Transparent.png';
+			return input;
+		};
+	});
