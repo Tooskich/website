269ff3bbcb551a171042c989a64482b0c3ba1157
seba-1511 <seba-1511@hotmail.com>
2014-01-10 19:52:56 +0100

    Used service for News. shuffled before carousel.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,5 +2,13 @@
 
 angular.module('websiteApp')
     .controller('MainCtrl', function($scope, News) {
-        $scope.news = News.getNews();
+        function shuffle(o) { //v1.0
+            for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+            return o;
+        };
+
+        var news = News.getNews();
+        $scope.news = news;
+        $scope.randFirstNews = shuffle(news.slice(0, 5));
+
     });
--- app/scripts/services/news.js
+++ app/scripts/services/news.js
@@ -27,7 +27,55 @@ angular.module('websiteApp')
 				title: 'Exemple 3 Longue',
 				author: 'Séb',
 				date: 1389283404,
-				content: 'Voilà un exemple de news un peu plus long. Elle devrait être coupée vers la fin, parce que le texte est décidement trop long.',
+				content: 'Voilà un exemple de news un peu plus long. Elle devrait être coupée vers la fin, parce que le texte est décidement trop long. Et avec du html : <img src="https://www.google.ch/logos/doodles/2014/annette-von-droste-hulshoffs-217th-birthday-5701007384248320.2-hp.jpg" />',
+			},{
+				id: 0,
+				title: 'Exemple 0',
+				author: 'Séb',
+				date: 1389283401,
+				content: 'Voilà un exemple de news. src="http://www.swiss-ski.ch/uploads/pics/Aerni_Luca_07.jpg"',
+			}, {
+				id: 1,
+				title: 'Exemple 1',
+				author: 'Séb',
+				date: 1389283402,
+				content: 'Voilà un exemple de news. src="http://www.swiss-ski.ch/uploads/pics/Aerni_Luca_07.jpg"',
+			}, {
+				id: 2,
+				title: 'Exemple 2',
+				author: 'Séb',
+				date: 1389283403,
+				content: 'Voilà un exemple de news.',
+			}, {
+				id: 3,
+				title: 'Exemple 3 Longue',
+				author: 'Séb',
+				date: 1389283404,
+				content: 'Voilà un exemple de news un peu plus long. Elle devrait être coupée vers la fin, parce que le texte est décidement trop long. Et avec du html : <img src="https://www.google.ch/logos/doodles/2014/annette-von-droste-hulshoffs-217th-birthday-5701007384248320.2-hp.jpg" />',
+			},{
+				id: 0,
+				title: 'Exemple 0',
+				author: 'Séb',
+				date: 1389283401,
+				content: 'Voilà un exemple de news. src="http://www.swiss-ski.ch/uploads/pics/Aerni_Luca_07.jpg"',
+			}, {
+				id: 1,
+				title: 'Exemple 1',
+				author: 'Séb',
+				date: 1389283402,
+				content: 'Voilà un exemple de news. src="http://www.swiss-ski.ch/uploads/pics/Aerni_Luca_07.jpg"',
+			}, {
+				id: 2,
+				title: 'Exemple 2',
+				author: 'Séb',
+				date: 1389283403,
+				content: 'Voilà un exemple de news.',
+			}, {
+				id: 3,
+				title: 'Exemple 3 Longue',
+				author: 'Séb',
+				date: 1389283404,
+				content: 'Voilà un exemple de news un peu plus long. Elle devrait être coupée vers la fin, parce que le texte est décidement trop long. Et avec du html : <img src="https://www.google.ch/logos/doodles/2014/annette-von-droste-hulshoffs-217th-birthday-5701007384248320.2-hp.jpg" />',
 			}, ];
 			if (!id) {
 				return news;
@@ -36,5 +84,5 @@ angular.module('websiteApp')
 			}
 		};
 
-		
+
 	});
