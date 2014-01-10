3489b63db03010b169692a5f903977a45b8ea038
seba-1511 <seba-1511@hotmail.com>
2014-01-10 19:11:33 +0100

    Added fiels for news service.

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -3,5 +3,12 @@
 angular.module('websiteApp')
 	.controller('NewsCtrl', function($scope, $routeParams) {
 		var newsId = $routeParams.id;
+		$scope.news = {
+            id: 3,
+            title: 'Exemple 3 Longue',
+            author: 'Séb',
+            date: 1389283404,
+            content: 'Voilà un exemple de news un peu plus long. Elle parce que le texte est décidement trop long. <a href="hahaha">devrait être coupée vers la fin</a>, <img src="https://www.google.ch/logos/doodles/2014/annette-von-droste-hulshoffs-217th-birthday-5701007384248320.2-hp.jpg" />',
+        };
 		$scope.disqusId = 'News' + newsId;
 	});
---
+++ app/scripts/services/news.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('websiteApp')
+  .service('News', function News() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
