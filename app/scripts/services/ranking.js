08542f21a10f0e9cb2db74ff1193164830d06748
seba-1511 <seba-1511@hotmail.com>
2014-01-11 16:49:13 +0100

    Added ranking factory files.

---
+++ app/scripts/services/ranking.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('websiteApp')
+  .factory('Ranking', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
