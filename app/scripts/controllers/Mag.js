32f64566eefe5261ae6ff86850dee7f2aa5d5d82
seba-1511 <seba-1511@hotmail.com>
2014-01-11 12:42:08 +0100

    Moved dist dir to the front.

--- app/scripts/controllers/Mag.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('websiteApp')
-  .controller('MagCtrl', function ($scope) {
-    
-  });
--- app/scripts/controllers/news.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('websiteApp')
-	.controller('NewsCtrl', function($scope, $routeParams, News) {
-		var newsId = $routeParams.id;
-		$scope.news = News.getNews(newsId);
-		$scope.disqusId = 'News' + newsId;
-	});
