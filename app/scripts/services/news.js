b86e4defdfeace1b9ec4a45028806c692883a52f
seba-1511 <seba-1511@hotmail.com>
2014-01-29 22:48:45 +0100

    Tried to link with backend. Failed.

--- app/scripts/services/news.js
+++ app/scripts/services/news.js
@@ -1,71 +1,37 @@
 'use strict';
 
 angular.module('websiteApp')
-    .factory('News', function() {
-        var news = [{
-            id: 0,
-            author: 'Séb',
-            title: 'News n°0',
-            content: 'Mon super text, avec du html: <img src="http://www.google.ch/logos/doodles/2014/annette-von-droste-hulshoffs-217th-birthday-5701007384248320.2-hp.jpg" /><a href="http://tooski.ch"></a>',
-            date: 1389390694,
-            mag: 1,
-        }, {
-            id: 1,
-            author: 'Séb',
-            title: 'News n°1',
-            content: 'Mon super text, avec du html: <img src="http://www.fricktal24.ch/typo3temp/pics/Ski-Aerni_Luca_01_55d138efee.jpg" /><a href="http://tooski.ch"></a>',
-            date: 1389390695,
-            mag: 1,
-        }, {
-            id: 2,
-            author: 'Séb',
-            title: 'News n°2',
-            content: 'Mon super text, avec du html: <a href="https://tooski.ch"><img src="http://skiweltcup.tv/wp-content/themes/tvsportnews/images/09-aerni002-klein-swiss-ski.jpg" /></a>',
-            date: 1389390696,
-            mag: 1,
-        }, {
-            id: 3,
-            author: 'Séb',
-            title: 'News n°0',
-            content: 'Mon super text, avec du html: <img src="http://www.google.ch/logos/doodles/2014/annette-von-droste-hulshoffs-217th-birthday-5701007384248320.2-hp.jpg" /><a href="http://tooski.ch"></a>',
-            date: 1389390694,
-            mag: 1,
-        }, {
-            id: 4,
-            author: 'Séb',
-            title: 'News n°1',
-            content: 'Mon super text, avec du html: <img src="http://www.fricktal24.ch/typo3temp/pics/Ski-Aerni_Luca_01_55d138efee.jpg" /><a href="http://tooski.ch"></a>',
-            date: 1389390695,
-            mag: 0,
-        }, {
-            id: 5,
-            author: 'Séb',
-            title: 'News n°2',
-            content: 'Mon super text, avec du html: <a href="https://tooski.ch"><img src="http://skiweltcup.tv/wp-content/themes/tvsportnews/images/09-aerni002-klein-swiss-ski.jpg" /></a>',
-            date: 1389390696,
-            mag: 0,
-        }, {
-            id: 6,
-            author: 'Séb',
-            title: 'News n°0',
-            content: 'Mon super text, avec du html: <img src="http://www.google.ch/logos/doodles/2014/annette-von-droste-hulshoffs-217th-birthday-5701007384248320.2-hp.jpg" /><a href="http://tooski.ch"></a>',
-            date: 1389390694,
-            mag: 1,
-        }, {
-            id: 7,
-            author: 'Séb',
-            title: 'News n°1',
-            content: 'Mon super text, avec du html: <img src="http://www.fricktal24.ch/typo3temp/pics/Ski-Aerni_Luca_01_55d138efee.jpg" /><a href="http://tooski.ch"></a>',
-            date: 1389390695,
-            mag: 1,
-        }, {
-            id: 8,
-            author: 'Séb',
-            title: 'News n°2',
-            content: 'Mon super text, avec du html: <a href="https://tooski.ch"><img src="http://skiweltcup.tv/wp-content/themes/tvsportnews/images/09-aerni002-klein-swiss-ski.jpg" /></a>',
-            date: 1389390696,
-            mag: 0,
-        }, ];
+    .factory('News', function($http) {
+        var news = [],
+            that = this;
+
+        window.JSON_CALLBACK = function(response) {
+
+            debugger;
+            that.news = response.map(function(el) {
+                var result = el.fields;
+                result.id = el.pk;
+                return result
+            });
+        };
+
+        $http.get(
+            'http://tooski.webfactional.com/api/news/get?callback', {
+                cache: 'true'
+            }
+        )
+            .success(function(response) {
+                debugger;
+                that.news = response.map(function(el) {
+                    var result = el.fields;
+                    result.id = el.pk;
+                    return result
+                });
+            })
+        // .error(function(status, response) {
+        //     debugger;
+        //     alert('Wrong connection with the server...');
+        // });
 
 
         // Public API here
