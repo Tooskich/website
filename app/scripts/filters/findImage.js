3b1949002e595f1b9f5321275b630ae5e13e6121
seba-1511 <seba-1511@hotmail.com>
2014-06-10 17:17:22 +0200

    changed default image.

--- app/scripts/filters/findImage.js
+++ app/scripts/filters/findImage.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('websiteApp')
-	.filter('findImage', function() {
-		return function(input) {
-			var re = /(https?:\/\/[A-Za-z0-9_/\.\-\(\)]*\.(?:png|jpg|svg|jpeg))/gi;
-			input = re.exec(input);
-			input = !! input ? input[0] : '/images/site/Sflocon_vecteur_Transparent.png';
-			return input;
-		};
-	});
+    .filter('findImage', function() {
+        return function(input) {
+            var re =
+                /(https?:\/\/[A-Za-z0-9_/\.\-\(\)]*\.(?:png|jpg|svg|jpeg))/gi;
+            input = re.exec(input);
+            input = !! input ? input[0] : '/images/site/tooski.png';
+            return input;
+        };
+    });
