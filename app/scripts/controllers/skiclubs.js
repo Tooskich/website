f36186edc8adea5aca9db7b84805bd12d6849cef
seba-1511 <seba-1511@hotmail.com>
2014-06-15 18:38:06 +0200

    Auto calibration of the goolge map.

--- app/scripts/controllers/skiclubs.js
+++ app/scripts/controllers/skiclubs.js
@@ -2,11 +2,31 @@
 
 angular.module('websiteApp')
     .controller('SkiclubsCtrl', function($scope) {
+        var latitude = 46.3514,
+            longitude = 7.1581,
+            setMapCenter;
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                latitude = position.coords.latitude;
+                longitude = position.coords.longitude;
+                setMapCenter();
+            });
+        }
+
+        setMapCenter = function() {
+            $scope.map.center = {
+                latitude: latitude,
+                longitude: longitude,
+            };
+        };
+
+
         $scope.map = {
             center: {
-                latitude: 45,
-                longitude: -73
+                latitude: latitude,
+                longitude: longitude,
             },
-            zoom: 8
+            zoom: 8,
         };
     });
