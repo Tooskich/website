defda50a041befd3c524acec34dfc6097d43a8c6
seba-1511 <seba-1511@hotmail.com>
2014-06-15 18:38:34 +0200

    Added files for skiclubs factory.

---
+++ app/scripts/services/skiclubs.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('websiteApp')
+  .factory('Skiclubs', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
