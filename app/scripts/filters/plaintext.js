b9f0974806fb1cbf514cc84072e92805e02d2470
seba-1511 <seba-1511@hotmail.com>
2014-06-24 18:27:30 +0200

    Improved plainText filter and fixed a minor bug.

--- app/scripts/filters/plaintext.js
+++ app/scripts/filters/plaintext.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('websiteApp')
-    .filter('plainText', function() {
-        return function(text) {
-        // text = String(text)
-        //     .replace(/<[^>]+>/gm, '');
-            return angular.element(text)
-                .text();
+    .filter('plainText', function($filter) {
+        return function(text, length, end) {
+            text = String(text)
+                .replace(/<[^>]+>|&nbsp;/gm, '');
+            // text = angular.element(text)
+            //     .text();
+            return length ? $filter('truncate')(text, length) : text;
         };
     });
