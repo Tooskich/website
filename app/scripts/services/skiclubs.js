f441c844e7352695fd3b9ee6834bc0b403f7e69a
seba-1511 <seba-1511@hotmail.com>
2014-06-25 23:21:03 +0200

    Added skiclubs connection to API.

--- app/scripts/services/skiclubs.js
+++ app/scripts/services/skiclubs.js
@@ -2,26 +2,14 @@
 
 angular.module('websiteApp')
     .factory('Skiclubs', function($http, Server) {
-        var skiclubs = [{
-            id: 1,
-            title: 'Les Diablerets',
-            latitude: 46.3514,
-            longitude: 7.1581,
-        }, {
-            id: 2,
-            title: 'Villars',
-            latitude: 46.3000,
-            longitude: 7.0500,
-        }, {
-            id: 3,
-            title: 'Leysin',
-            latitude: 46.3333,
-            longitude: 7.0000,
-        }, ];
+        var scApi = Server.Url + 'apiv1/skiclubs/';
 
         return {
             get: function(callback) {
-                callback(skiclubs);
+                $http.get(scApi)
+                    .then(function(res) {
+                        callback(res.data);
+                    }, Server.errorHandler);
             },
         };
     });
