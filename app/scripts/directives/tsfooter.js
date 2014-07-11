f77912fbec228d374765810cf4cf7667eebe288c
seba-1511 <seba-1511@hotmail.com>
2014-07-11 19:01:11 +0200

    Implemented bottom ad.

--- app/scripts/directives/tsfooter.js
+++ app/scripts/directives/tsfooter.js
@@ -1,13 +1,17 @@
 'use strict';
 
 angular.module('websiteApp')
-    .directive('tsFooter', function(Page) {
+    .directive('tsFooter', function(Page, Pub) {
         return {
             templateUrl: 'views/directives/core/footer.html',
             restrict: 'EACM',
             link: function postLink(scope, element, attrs) {
                 Page.getPageLinks(function(links) {
                     scope.pages = links;
+                });
+
+                Pub.getHorizontalBanner(function(banners) {
+                    scope.banner = banners[0];
                 });
             }
         };
