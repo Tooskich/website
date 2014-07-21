27bf98bd32a91f6fa9d57c54dd740a7d21de50d9
seba-1511 <seba-1511@hotmail.com>
2014-07-21 12:55:56 +0200

    Hopefully fixed the mgMin problem with skiclubs page.

--- app/scripts/controllers/skiclubs.js
+++ app/scripts/controllers/skiclubs.js
@@ -1,55 +1,57 @@
 'use strict';
 
 angular.module('websiteApp')
-    .controller('SkiclubsCtrl', function($scope, Skiclubs, Widget) {
-        var latitude = 46.3514,
-            longitude = 7.1581,
-            setMapCenter;
+    .controller('SkiclubsCtrl', ['$scope', 'Skiclubs', 'Widget',
+        function($scope, Skiclubs, Widget) {
+            var latitude = 46.3514,
+                longitude = 7.1581,
+                setMapCenter;
 
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                latitude = position.coords.latitude;
-                longitude = position.coords.longitude;
-                setMapCenter();
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    latitude = position.coords.latitude;
+                    longitude = position.coords.longitude;
+                    setMapCenter();
+                });
+            }
+
+            setMapCenter = function() {
+                $scope.map.center = {
+                    latitude: latitude,
+                    longitude: longitude,
+                };
+            };
+
+            Skiclubs.get(function(data) {
+                $scope.skiclubs = data;
+            });
+
+            Widget.get(2, function(widget) {
+                $scope.widgetContent = widget;
+            });
+
+            $scope.map = {
+                center: {
+                    latitude: latitude,
+                    longitude: longitude,
+                },
+                zoom: 8,
+                draggable: true,
+            };
+
+            $scope.markerClick = function($markerModel) {
+                $scope.$emit('tsChangeClub', $markerModel);
+            };
+
+            $scope.currentClub = {
+                title: '',
+                content: '',
+                contact: '',
+            };
+
+            $scope.$on('tsChangeClub', function(event, input) {
+                $scope.currentClub = input;
+                $scope.$apply();
             });
         }
-
-        setMapCenter = function() {
-            $scope.map.center = {
-                latitude: latitude,
-                longitude: longitude,
-            };
-        };
-
-        Skiclubs.get(function(data) {
-            $scope.skiclubs = data;
-        });
-
-        Widget.get(2, function(widget) {
-            $scope.widgetContent = widget;
-        });
-
-        $scope.map = {
-            center: {
-                latitude: latitude,
-                longitude: longitude,
-            },
-            zoom: 8,
-            draggable: true,
-        };
-
-        $scope.markerClick = function($markerModel) {
-            $scope.$emit('tsChangeClub', $markerModel);
-        };
-
-        $scope.currentClub = {
-            title: '',
-            content: '',
-            contact: '',
-        };
-
-        $scope.$on('tsChangeClub', function(event, input) {
-            $scope.currentClub = input;
-            $scope.$apply();
-        });
-    });
+    ]);
