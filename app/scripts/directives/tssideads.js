4c7f8fbb94f30af3191a419f9ffb35342f2467a1
seba-1511 <seba-1511@hotmail.com>
2015-01-08 19:59:02 +0100

    Added side ads template.

--- app/scripts/directives/tssideads.js
+++ app/scripts/directives/tssideads.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('websiteApp')
-  .directive('tsSideAds', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the tsSideAds directive');
-      }
-    };
-  });
+    .directive('tsSideAds', function() {
+        return {
+            template: '<div>Side Ads !</div>',
+            restrict: 'EACM',
+            link: function postLink(scope, element, attrs) {
+                element.text('this is the tsSideAds directive');
+            }
+        };
+    });
