97a326daa0588d40f542c20973f166c74425443b
Aymeric Bringard <contact@aymericb.com>
2016-03-09 23:55:55 +0100

    add min-width rule to tsAdPlaceholder directive

--- app/scripts/directives/tsAdPlaceholder.js
+++ app/scripts/directives/tsAdPlaceholder.js
@@ -1,22 +1,41 @@
 'use strict';
 
 angular.module('websiteApp')
-    .directive('tsAdPlaceholder', ['Pub',
-        function(Pub) {
+    .directive('tsAdPlaceholder', ['$window', 'Pub',
+        function($window, Pub) {
             return {
                 templateUrl: 'views/directives/core/placeholder.html',
                 restrict: 'EACM',
                 replace: true,
+                scope: {
+                    width: '=?minWidth',
+                },
                 link: function postLink(scope, element, attrs) {
+
+                    var checkWidth = function(width) {
+                        var width = width;
+                        return function() {
+                            if (width >= window.innerWidth) {
+                                element.css('display', 'none');
+                            }
+                            else {
+                                element.css('display', 'block');
+                            }
+                        };
+                    };
+                    checkWidth = checkWidth(scope.width);
+                    checkWidth();
+
                     var pubs = Pub.getPlaceholder(
                         attrs.placeholder,
                         attrs.category,
                         function (pubs) {
                             scope.ad = pubs[0];
                             scope.cat = attrs.category;
-                            console.log(pubs);
                         }
                     );
+
+                    angular.element($window).on('resize', checkWidth);
                 }
             };
         }
