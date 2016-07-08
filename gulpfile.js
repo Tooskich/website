06ee97b5ac758250a3fd2a2617617d1a7490576b
Aymeric Bringard <contact@aymericb.com>
2016-07-08 17:14:26 +0200

    add gulp bower task and npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ var gulp = require('gulp');
 // Load plugins
 var $ = require('gulp-load-plugins')();
 
+// Config
+var config = {
+     bowerDir: './app/bower_components' ,
+     cssPath: './dist/styles',
+     htmlPath: './app',
+     jsPath: './app/scripts',
+     sassPath: './app/styles',
+}
 
 gulp.task('connectDev', function () {
   $.connect.server({
@@ -20,41 +28,49 @@ gulp.task('connectDist', function () {
   });
 });
 
+gulp.task('bower', function() { 
+    return $.bower()
+         .pipe(gulp.dest(config.bowerDir)) ;
+});
+
 gulp.task('html', function () {
-    return gulp.src('app/*.html')
+    return gulp.src(config.htmlPath + '/*.html')
         .pipe($.connect.reload());
 });
 
 gulp.task('styles', function () {
-    return gulp.src('app/styles/main.scss')
+    return gulp.src(config.sassPath + '/main.scss')
         .pipe($.sass({
-          style: 'expanded',
-          loadPath: ['app/bower_components']
+            style: 'expanded',
+            loadPath: [
+                config.sassPath,
+                config.bowerDir
+            ]
         }))
-        .pipe(gulp.dest('app/styles'))
-        .pipe($.minifycss())
-        .pipe(gulp.dest('dist/styles'))
+        .pipe(gulp.dest(config.sassPath))
+        .pipe($.cleanCss())
+        .pipe(gulp.dest(config.cssPath))
         .pipe($.connect.reload());
 });
 
 gulp.task('scripts', function() {
-    return gulp.src(['app/scripts/*.js', 'app/scripts/**/*.js'])
+    return gulp.src([config.jsPath + '/*.js', config.jsPath + '/**/*.js'])
         .pipe($.uglify('app.min.js', {
             outSourceMap: true
         }))
-        .pipe(gulp.dest('dist/scripts'));
+        .pipe(gulp.dest('dist/scripts'))
         .pipe($.connect.reload());
 });
 
 gulp.task('watch', function () {
     // Watch .html files
-    gulp.watch(['app/*.html'], ['html']);
+    gulp.watch([config.htmlPath + '/*.html'], ['html']);
 
     // Watch .scss files
-    gulp.watch(['app/styles/**/*.scss'], ['styles']);
+    gulp.watch([config.sassPath + '/**/*.scss'], ['styles']);
 
     // Watch .js files
-    gulp.watch(['app/scripts/*.js', 'app/scripts/**/*.js'], ['scripts']);
+    gulp.watch([config.jsPath + '/*.js', config.jsPath + '/**/*.js'], ['scripts']);
 });
 
 gulp.task('clean', function () {
@@ -63,7 +79,7 @@ gulp.task('clean', function () {
 });
 
 // Build
-gulp.task('build', ['clean', 'html', 'styles', 'scripts']);
+gulp.task('build', ['clean', 'bower', 'html', 'styles', 'scripts']);
 
 // Default task
 gulp.task('default', ['clean', 'build', 'connectDev', 'watch']);
