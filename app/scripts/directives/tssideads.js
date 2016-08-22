f2cc7eb10531aea339057642e683186ecdce1275
diadzine <diadzine@gmail.com>
2016-08-22 19:51:24 +0200

    Fixing DI

--- app/scripts/directives/tssideads.js
+++ app/scripts/directives/tssideads.js
@@ -1,41 +1,43 @@
 'use strict';
 
 angular.module('websiteApp')
-    .directive('tsSideAds', function($window, Pub) {
-        return {
-            templateUrl: 'views/directives/core/sideads.html',
-            restrict: 'EACM',
-            scope: {
-                width: '=minWidth',
-            },
-            link: function postLink(scope, element, attrs) {
-                function shuffle(o) { //v1.0
-                    for (var j, x, i = o.length; i; j = Math.floor(Math.random() *
-                        i), x = o[--i], o[i] = o[j], o[j] = x);
-                    return o;
-                };
-
-                var checkWidth = function(width) {
-                    var width = width;
-                    return function() {
-                        if (width >= window.innerWidth) {
-                            element.css('display', 'none');
-                        }
-                        else {
-                            element.css('display', 'block');
-                        }
+    .directive('tsSideAds', ['$window', 'Pub',
+        function($window, Pub) {
+            return {
+                templateUrl: 'views/directives/core/sideads.html',
+                restrict: 'EACM',
+                scope: {
+                    width: '=minWidth',
+                },
+                link: function postLink(scope, element, attrs) {
+                    function shuffle(o) { //v1.0
+                        for (var j, x, i = o.length; i; j = Math.floor(Math.random() *
+                            i), x = o[--i], o[i] = o[j], o[j] = x);
+                        return o;
                     };
-                };
-                checkWidth = checkWidth(scope.width);
-                checkWidth();
 
-                Pub.getVerticalBanner(function(banners) {
-                    scope.ad = shuffle(banners)[0];
-                });
+                    var checkWidth = function(width) {
+                        var width = width;
+                        return function() {
+                            if (width >= window.innerWidth) {
+                                element.css('display', 'none');
+                            }
+                            else {
+                                element.css('display', 'block');
+                            }
+                        };
+                    };
+                    checkWidth = checkWidth(scope.width);
+                    checkWidth();
+
+                    Pub.getVerticalBanner(function(banners) {
+                        scope.ad = shuffle(banners)[0];
+                    });
 
 
-                angular.element($window)
-                    .on('resize', checkWidth);
-            }
-        };
-    });
+                    angular.element($window)
+                        .on('resize', checkWidth);
+                }
+            };
+        }
+    ]);
